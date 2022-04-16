fce48e977a5d0802361ebed0ed0a2a00d5fdf33d
Marko Nikolajevic <mrknikolajevic@gmail.com>
2022-04-16 21:49:59 +0200

    add loading spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import EmptyInvoice from '../components/EmptyInvoice';
 import InvoicePreview from '../components/InvoicePreview';
 import { AnimatePresence } from 'framer-motion';
 import NewInvoiceBackdrop from '../components/NewInvoiceBackdrop';
+import LoadingIcon from '../components/LoadingIcon';
 
 const Home: NextPage = () => {
   const { invoices } = useContext(InvoiceContext);
@@ -15,13 +16,17 @@ const Home: NextPage = () => {
     <main className='relative w-full'>
       <div className='mx-6 mt-8 flex flex-col md:mx-12 md:mt-14 lg:mx-auto lg:mt-18 lg:w-4/5'>
         <Header setShowAddInvoice={setShowAddInvoice} />
-        {!invoices && <div>Loading...</div>}
         {invoices?.data.length > 0 && <InvoicePreview />}
         {invoices?.data.length === 0 && <EmptyInvoice />}
       </div>
       <AnimatePresence>
         {showAddInvoice && <NewInvoiceBackdrop setShowAddInvoice={setShowAddInvoice} />}
       </AnimatePresence>
+      {!invoices && (
+        <div className='absolute top-0 left-0 right-0 flex h-screen items-center justify-center  bg-[#000000e1]'>
+          <LoadingIcon />
+        </div>
+      )}
     </main>
   );
 };
