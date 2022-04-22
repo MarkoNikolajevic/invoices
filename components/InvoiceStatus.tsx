65b82cee1a5beb8dbafa106ecc6bd602be1da08a
Marko Nikolajevic <mrknikolajevic@gmail.com>
2022-04-22 22:49:55 +0200

    fix status undefined

--- components/InvoiceStatus.tsx
+++ components/InvoiceStatus.tsx
@@ -6,9 +6,9 @@ const InvoiceStatus = ({ invoice }: { invoice: Invoice }) => {
     <div
       className={cn(
         'flex h-10 w-[6.5rem] items-center justify-center rounded-md',
-        invoice.status === 'paid'
+        invoice?.status === 'paid'
           ? 'bg-status-paid/[0.06]'
-          : invoice.status === 'pending'
+          : invoice?.status === 'pending'
           ? 'bg-status-pending/[0.06]'
           : 'bg-status-draft/[0.06]'
       )}
@@ -16,15 +16,15 @@ const InvoiceStatus = ({ invoice }: { invoice: Invoice }) => {
       <p
         className={cn(
           'flex items-center text-xs font-bold capitalize',
-          invoice.status === 'paid'
+          invoice?.status === 'paid'
             ? 'text-status-paid'
-            : invoice.status === 'pending'
+            : invoice?.status === 'pending'
             ? 'text-status-pending'
             : 'test-status-draft'
         )}
       >
         <span className='mr-2 block h-2 w-2 rounded-full bg-current'></span>
-        {invoice.status}
+        {invoice?.status}
       </p>
     </div>
   );
